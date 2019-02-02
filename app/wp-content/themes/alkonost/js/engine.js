06b90078891b83173e996496f981891850459e3c
Aleksey <kronos2003@gmail.com>
2019-02-02 12:57:29 +0200

    Добавлены шрифты, слайдер в "о гастрономе" на главной

--- app/wp-content/themes/alkonost/js/engine.js
+++ app/wp-content/themes/alkonost/js/engine.js
@@ -1,25 +1,49 @@
 $(document).ready(function(){
-
-
-	// mobile-menu
-	$('#navbar').each(function(){
-		var $this = $(this),
-			$link = $('.navbar-toggle'),
-			$close = $('.close-menu'),
-
-			init = function(){
-				$link.on('click', openMenu);
-				$close.on('click', closeMenu);
+	$('#about-slider').slick({
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		// appendArrows: '.top-goods-nav',
+		prevArrow : '<button type="button" class="slick-prev" aria-label="Назад"><img src="/wp-content/themes/alkonost/images/about-arrow.svg" /></button>',
+		nextArrow: '<button class="slick-next" aria-label="Вперед" type="button"><img src="/wp-content/themes/alkonost/images/about-arrow.svg" /></button>',
+		// autoplay: true,
+		// autoplaySpeed: 2000,
+		responsive: [
+			{
+			  breakpoint: 1199,
+			  settings: {
+			    slidesToShow: 3
+			  }
 			},
-			openMenu = function(e){
-				e.preventDefault();
-				$('body').addClass('o-menu');
-
+			{
+			  breakpoint: 767,
+			  settings: {
+			    slidesToShow: 2
+			  }
 			},
-			closeMenu = function(e){
-				e.preventDefault();
-				$('body').removeClass('o-menu');
-			};
-		init();
+			{
+			  breakpoint: 501,
+			  settings: {
+			    slidesToShow: 1,
+			    centerMode: true,
+			    centerPadding: '110px'
+			  }
+			},
+			{
+			  breakpoint: 450,
+			  settings: {
+			    slidesToShow: 1,
+			    centerMode: true,
+			    centerPadding: '110px'
+			  }
+			},
+			{
+			  breakpoint: 450,
+			  settings: {
+			    slidesToShow: 1,
+			    centerMode: true,
+			    centerPadding: '50px'
+			  }
+			}
+		]
 	});	
 });
