fec05302c175b693390a45cdb9ca90ad7919db0a
Aleksey <kronos2003@gmail.com>
2019-02-03 21:38:37 +0200

    Адаптив главной

--- app/wp-content/themes/alkonost/js/engine.js
+++ app/wp-content/themes/alkonost/js/engine.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
 	$('#about-slider').slick({
 		slidesToShow: 3,
 		slidesToScroll: 1,
-		// appendArrows: '.top-goods-nav',
 		prevArrow : '<button type="button" class="slick-prev" aria-label="Назад"><img src="/wp-content/themes/alkonost/images/about-arrow.svg" alt="" /></button>',
 		nextArrow: '<button class="slick-next" aria-label="Вперед" type="button"><img src="/wp-content/themes/alkonost/images/about-arrow.svg" alt="" /></button>',
 		// autoplay: true,
@@ -21,8 +20,9 @@ $(document).ready(function(){
 			  }
 			},
 			{
-			  breakpoint: 501,
+			  breakpoint: 550,
 			  settings: {
+			  	arrows: false,
 			    slidesToShow: 1,
 			    centerMode: true,
 			    centerPadding: '110px'
@@ -31,6 +31,7 @@ $(document).ready(function(){
 			{
 			  breakpoint: 450,
 			  settings: {
+			  	arrows: false,
 			    slidesToShow: 1,
 			    centerMode: true,
 			    centerPadding: '110px'
@@ -39,6 +40,7 @@ $(document).ready(function(){
 			{
 			  breakpoint: 450,
 			  settings: {
+			  	arrows: false,
 			    slidesToShow: 1,
 			    centerMode: true,
 			    centerPadding: '50px'
@@ -50,46 +52,91 @@ $(document).ready(function(){
 	$('#lastnews-carousel').slick({
 		slidesToShow: 5,
 		slidesToScroll: 1,
-		// appendArrows: '.top-goods-nav',
 		prevArrow : '<button type="button" class="slick-prev" aria-label="Назад"></button>',
 		nextArrow: '<button class="slick-next" aria-label="Вперед" type="button"></button>',
 		// autoplay: true,
 		// autoplaySpeed: 2000,
 		responsive: [
 			{
-			  breakpoint: 1199,
+			  breakpoint: 1850,
+			  settings: {
+			    slidesToShow: 4
+			  }
+			},
+			{
+			  breakpoint: 1500,
 			  settings: {
 			    slidesToShow: 3
 			  }
 			},
 			{
-			  breakpoint: 767,
+			  breakpoint: 1100,
 			  settings: {
-			    slidesToShow: 2
+			    slidesToShow: 1,
+			    centerMode: true,
+			    centerPadding: '250px'
+			  }
+			},
+			{
+			  breakpoint: 870,
+			  settings: {
+			    slidesToShow: 1,
+			    centerMode: true,
+			    centerPadding: '150px'
+			  }
+			},
+			{
+			  breakpoint: 680,
+			  settings: {
+			  	arrows: false,
+			    slidesToShow: 1,
+			    centerMode: true,
+			    centerPadding: '110px'
+			  }
+			},
+			{
+			  breakpoint: 600,
+			  settings: {
+			  	arrows: false,
+			    slidesToShow: 1,
+			    centerMode: true,
+			    centerPadding: '75px'
 			  }
 			},
 			{
 			  breakpoint: 501,
 			  settings: {
+		  		arrows: false,
 			    slidesToShow: 1,
 			    centerMode: true,
-			    centerPadding: '110px'
+			    centerPadding: '80px'
 			  }
 			},
 			{
 			  breakpoint: 450,
 			  settings: {
-			    slidesToShow: 1,
-			    centerMode: true,
-			    centerPadding: '110px'
-			  }
-			},
-			{
-			  breakpoint: 450,
-			  settings: {
+		  		arrows: false,
 			    slidesToShow: 1,
 			    centerMode: true,
 			    centerPadding: '50px'
+			  }
+			},
+			{
+			  breakpoint: 410,
+			  settings: {
+		  		arrows: false,
+			    slidesToShow: 1,
+			    centerMode: true,
+			    centerPadding: '40px'
+			  }
+			},
+			{
+			  breakpoint: 350,
+			  settings: {
+		  		arrows: false,
+			    slidesToShow: 1,
+			    centerMode: true,
+			    centerPadding: '25px'
 			  }
 			}
 		]
